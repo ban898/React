2d252959f9b16fc5a8ce4c21bab11f42d1402400
ban898 <davidface2@gmail.com>
2022-09-26 16:01:08 +0300

    Added 2 way binding and Some components

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,43 +1,49 @@
 import "./ExpenseForm.css";
 import { useState } from "react";
 
-const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
   const titleChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
-    });
+    setEnteredTitle(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+
+    setEnteredAmount("");
+    setEnteredDate("");
+    setEnteredTitle("");
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -45,6 +51,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -54,6 +61,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
@@ -66,3 +74,38 @@ const ExpenseForm = () => {
 };
 
 export default ExpenseForm;
+
+//-------------------------------------------------------------------------------------
+//Another approach
+//To useState but in one line
+// const [userInput, setUserInput] = useState({
+//   enteredTitle: "",
+//   enteredAmount: "",
+//   enteredDate: "",
+// });
+
+// setUserInput({
+//   ...userInput,
+//   enteredTitle: event.target.value,
+// });
+
+// setUserInput((prevState) => {
+//   return { ...prevState, enteredTitle: event.target.value };
+// });
+
+// setUserInput({
+//   ...userInput,
+//   enteredDate: event.target.value,
+// });
+// setUserInput({
+//   ...userInput,
+//   enteredAmount: event.target.value,
+// });
+
+//-------------------------------------------------------------------------------------
+//We add value prop because we want to reflect the change we did with the state on the input field
+//  <input
+//      type="text"
+//      value={enteredTitle}
+//      onChange={titleChangeHandler}
+//  />
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,51 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
 
 export default NewExpense;
+
+//-------------------------------------------------------------------------------------
+//this function we pass into the child which in this case is ExpenseForm
+//we pass it as a prop in the line
+//<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+//Hence to that we can get access to eneteredExpenseData because this data 'lives' there
+//then we create a new object with that data and adding an id field to that object
+//Inside of newExpense this is what we do :
+
+//   const saveExpenseDataHandler = (enteredExpenseData) => {
+//     const expenseData = {
+//       ...enteredExpenseData,
+//       id: Math.random().toString(),
+//     };
+//     console.log(expenseData);
+//   };
+
+//   return (
+//     <div className="new-expense">
+//       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+//     </div>
+//   );
+// };
+
+//Then inside of the child component which in this case is ExpenseForm
+//We do this :
+
+//props.onSaveExpenseData(expenseData);
+
+//we get access to the props [because we sent props] and we call the function by the name [not the value we passed] [onSaveExpenseData]
+//inside of the child and we sending the values we want to send "up"
